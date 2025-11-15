f24dd73d241b4773279a26f895aeac030a9564f9
andreykarganov <karganov.an@yandex.ru>
2025-11-15 19:58:28 +0300

    changed title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
             <Route path="/skills" element={<SkillList />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<Home />} />
+            c
           </Routes>
         </main>
         <Footer />
